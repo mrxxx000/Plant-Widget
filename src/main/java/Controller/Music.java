f0c3bb083b55b58d0bf9efa14bbfaa816c88967e
Akmal Safi <>
2024-05-18 01:48:26 +0200

    added music

---
+++ src/main/java/Controller/Music.java
@@ -0,0 +1,21 @@
+package Controller;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+
+public class Music {
+    public static void playMusic(String path) {
+        try {
+            File file = new File(path);
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file.toURI().toURL());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        } catch (Exception e) {
+            System.out.println("Error with playing sound.");
+            e.printStackTrace();
+        }
+    }
+}
