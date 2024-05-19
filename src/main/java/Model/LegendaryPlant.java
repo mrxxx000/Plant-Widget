3b8ad2f5cdfb8a0b5efd2ad38601463bf7e569f0
Yrja Mai Hoang <Yrjamai@outlook.com>
2024-05-19 16:56:28 +0200

    Comments
    Added comments and author tags.

--- src/main/java/Model/LegendaryPlant.java
+++ src/main/java/Model/LegendaryPlant.java
@@ -1,16 +1,22 @@
 package Model;
 
 import javafx.scene.image.Image;
+
 import java.io.Serializable;
 
 /**
- * This is pretty much a static object. It should never be changed after it is created unless it is being removed.
+ * This class is used to create a legendary plant object.
+ * It contains the name, type and image of the legendary plant.
+ * A Legendary plant is static, in the sense that it does not need maintenance.
+ *
+ * @author Yrja Mai Hoang
  */
 public class LegendaryPlant implements Serializable {
     private String name;
     private PlantTypes type;
     private Image image;
-    public LegendaryPlant(){
+
+    public LegendaryPlant() {
 
     }
 
@@ -25,12 +31,15 @@ public class LegendaryPlant implements Serializable {
     public void setTypes(PlantTypes type) {
         this.type = type;
     }
+
     public Image getImage() {
         return image;
     }
+
     public String getName() {
         return name;
     }
+
     public PlantTypes getType() {
         return type;
     }
--- src/main/java/Model/PlantTypes.java
+++ src/main/java/Model/PlantTypes.java
@@ -1,5 +1,9 @@
 package Model;
 
+/**
+ * Enum class for the different types of plants that can be created.
+ * @author Yrja Mai Hoang
+ */
 public enum PlantTypes {
     PUMPKIN,
     SUNFLOWER,
